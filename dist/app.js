ead624af4a387bf4d3df93c29b57186c6e962614
nickroberts404 <nickroberts404@gmail.com>
2017-02-22 19:59:01 -0600

    Fixed one more teeny walkthrough error

--- dist/app.js
+++ dist/app.js
@@ -31831,7 +31831,7 @@ var thoughts = exports.thoughts = [{
 }, {
 	id: 4,
 	title: "Writing Thoughts",
-	description: "Thoughtjar uses a small subset of features from Markdown, a text-to-HTML conversion tool that makes writing for the web easier!\n\n *Line Break:* To create a line break, hit enter twice.\n\n*Bold:* To make a section of text bold, wrap it in double asterisks. \\*\\*I'm bold!\\*\\* becomes **I'm bold!**.\n\n*Emphasis:* To emphasize a section of text, make it italic by wrapping it in single asterisks. \\*I'm emphasized!\\* becomes *I'm emphasized!*\n\n*Links:* To create a hyperlink from a section of text, wrap the text in brackets and follow it with the URL wrapped in parentheses. \\[Visit meadowlab.io](https://meadowlab.io) becomes [Visit meadowlab.io](http://meadowlab.io).",
+	description: "Thoughtjar uses a small subset of features from Markdown, a text-to-HTML conversion tool that makes writing for the web easier!\n\n *Line Break:* To create a line break, hit enter twice.\n\n*Bold:* To make a section of text bold, wrap it in double asterisks. \\*\\*I'm bold!\\*\\* becomes **I'm bold!**.\n\n*Emphasis:* To emphasize a section of text, make it italic by wrapping it in single asterisks. \\*I'm emphasized!\\* becomes *I'm emphasized!*\n\n*Links:* To create a hyperlink from a section of text, wrap the text in brackets and follow it with the URL wrapped in parentheses. \\[Visit meadowlab.io](http://meadowlab.io) becomes [Visit meadowlab.io](http://meadowlab.io).",
 	category: "cat1468028016486",
 	createdOn: Date.now() - 4
 }, {
